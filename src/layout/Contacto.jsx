29bc98f2ebc631a74cc195783df5b8012793e008
FlorVarangot <majoreyes87@gmail.com>
2025-07-16 16:53:20 -0300

    Update keys mailjs

--- src/layout/Contacto.jsx
+++ src/layout/Contacto.jsx
@@ -9,14 +9,6 @@ const Contacto = ({ cart, borrar, vaciar }) => {
     const formRef = useRef(null);
     const [errors, setErrors] = useState({})
 
-    // const fecha = new Date().toLocaleString('es-AR', {
-    //     day: '2-digit',
-    //     month: '2-digit',
-    //     year: 'numeric',
-    //     hour: '2-digit',
-    //     minute: '2-digit'
-    // })
-
     const [formData, setFormData] = useState({
         name: '',
         mail: '',
@@ -51,47 +43,50 @@ const Contacto = ({ cart, borrar, vaciar }) => {
 
         if (!validateForm()) return;
 
-        emailjs.sendForm('service_9niqfrd', 'template_ie6t8dk', formRef.current, '7DgCJCkh7EW_fOd13')
-            .then(() => {
-                const fecha = new Date().toLocaleString('es-AR', {
-                    day: '2-digit',
-                    month: '2-digit',
-                    year: 'numeric',
-                    hour: '2-digit',
-                    minute: '2-digit'
-                })
-
-                emailjs.send(
-                    'service_9niqfrd',
-                    'template_yj6t84v',
-                    {
-                        to_email: 'florencia.rvarangot@gmail.com',
-                        reply_to: formData.mail,
-                        name: formData.name,
-                        mail: formData.mail,
-                        mensaje: formData.mensaje,
-                        suscripcion: formData.suscripcion ? '✅ Quiero recibir info' : '❌ No quiero recibir info',
-                        fecha
-                    },
-                    '7DgCJCkh7EW_fOd13'
-                )
-
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Mensaje enviado',
-                    text: 'Gracias por contactarte. Te responderemos pronto.',
-                    confirmButtonColor: 'var(--third-color)'
-                })
-
-                setFormData({
-                    name: '',
-                    mail: '',
-                    mensaje: '',
-                    suscripcion: false
-                })
-
-                setErrors({})
+        emailjs.sendForm(
+            import.meta.env.VITE_EMAILJS_SERVICE,
+            import.meta.env.VITE_EMAILJS_TEMPLATE,
+            formRef.current,
+            import.meta.env.VITE_EMAILJS_USER
+        ).then(() => {
+            const fecha = new Date().toLocaleString('es-AR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit'
             })
+
+            emailjs.send(
+                import.meta.env.VITE_EMAILJS_SERVICE,
+                import.meta.env.VITE_EMAILJS_TEMPLATE_ADMIN,
+                 {
+                    to_email: 'florencia.rvarangot@gmail.com',
+                    reply_to: formData.mail,
+                    name: formData.name,
+                    mail: formData.mail,
+                    mensaje: formData.mensaje,
+                    suscripcion: formData.suscripcion ? '✅ Quiero recibir info' : '❌ No quiero recibir info',
+                    fecha
+                },
+                import.meta.env.VITE_EMAILJS_USER
+            )
+            Swal.fire({
+                icon: 'success',
+                title: 'Mensaje enviado',
+                text: 'Gracias por contactarte. Te responderemos pronto.',
+                confirmButtonColor: 'var(--third-color)'
+            })
+
+            setFormData({
+                name: '',
+                mail: '',
+                mensaje: '',
+                suscripcion: false
+            })
+
+            setErrors({})
+        })
             .catch((error) => {
                 Swal.fire({
                     icon: 'error',
